913efb647a7949280e8855ce7a92eb7c1b242db1
S0r4 <vedantairon@gmail.com>
2025-07-09 11:31:05 +0530

    Port fixed

--- frontend/src/app/https/api.js
+++ frontend/src/app/https/api.js
@@ -1,8 +1,12 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL, // Assuming your backend is running on this URL
-  //timeout: 10000, // Timeout of 5 seconds
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+    Accept: 'application/json',
+  },
+  withCredentials: true,
 });
 
 export default instance;
